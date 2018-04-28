848dec2c30917da6045e7558a7f8cd4f882afd97
ChrisScheidig <c@scheidig.net>
2018-04-28 02:15:20 +0200

    Added extern lib matter.js
    Trying to use matter.js as box2d is old as fuck

--- app/minigolf.js
+++ app/minigolf.js
@@ -1,14 +1,24 @@
 import * as Rendering from './renderer.js';
 
 export var Minigolf = (function(){
-	// vars
-	let renderer = Rendering.CreateRenderer();	
-	return {
-		update: function(delta) {
 
+	let renderer = Rendering.CreateRenderer();	
+	
+	// world defintion with objects
+	let myWorld = {
+		staticObjects: [],
+		addBorder: function(data) {
+			this.staticObjects.push(data);
+		}
+	};
+	// public functions and variables
+	return {
+		world:myWorld,
+		update: function(delta) {
+			// update game with constant delta ...
 		},
-		render: function() {
-			renderer.drawAll();
+		render: function() {		
+			renderer.drawAll(this.world.staticObjects);	
 		}
 	}
 })();
