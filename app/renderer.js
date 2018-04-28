412bc4eb1a4fb3d2d3d97013c7b34c337c1d5cae
ChrisScheidig <c@scheidig.net>
2018-04-28 02:53:37 +0200

    Added shooting line (ball-mouse)

--- app/renderer.js
+++ app/renderer.js
@@ -4,10 +4,27 @@ export function CreateRenderer(setup) {
 
 	// init visuals
 	let canvas = document.createElement("canvas");
-	let ctx = canvas.getContext("2d");				
+	let ctx = canvas.getContext("2d");	
+	let mousePos = null;
 	canvas.width = 500;					
 	canvas.height = 300;				
 	document.body.appendChild(canvas);	 
+	let playerBall = null;
+	
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+	}	
+	window.addEventListener('mousemove', function(evt) {	
+		mousePos = null;
+	}, true);		
+	canvas.addEventListener('mousemove', function(evt) {	
+		mousePos = getMousePos(canvas, evt);
+	}, false);	
+
 	
 	// variables
 	
@@ -20,9 +37,16 @@ export function CreateRenderer(setup) {
 			ctx.stroke(); 						
 		},
 		endFrame:function() {
-			
+			/// draw shooting line
+			if (mousePos && playerBall) {
+				ctx.beginPath();
+				ctx.moveTo(playerBall.x, playerBall.y);
+				ctx.lineTo(mousePos.x, mousePos.y);
+				ctx.stroke();
+			}
 		},
 		drawBalls:function(objects) {
+			playerBall = objects[0];
 			for (var obj of objects) {
 				ctx.beginPath();
 				ctx.arc(obj.x, obj.y, obj.radius, 0, 2 * Math.PI, false);
